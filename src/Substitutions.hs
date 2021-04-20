5866d92e9c2eccecf8e0fe3c4db68731f3ddaada
makspll <makspl17@gmail.com>
2021-04-20 15:47:35 +0100

    refactored substitutions

--- src/Substitutions.hs
+++ src/Substitutions.hs
@@ -2,6 +2,7 @@
 module Substitutions where
 import Common ( Exp(..) )
 import qualified Data.Set as Set
+import Control.Monad (msum)
 
 data Sub = Exp :\: Exp deriving (Ord, Show, Eq)
 
@@ -22,9 +23,12 @@ subSet subs exp
     | otherwise = exp
 
 
+subColides ::  Set.Set Sub-> Sub -> Bool
+subColides o (s :\: source) = Set.member source vars
+    where vars = getVars o
 
 mergeSub :: Sub -> Set.Set Sub -> Set.Set Sub
-mergeSub al be = Set.union a' b'
+mergeSub al be = nubSub $ Set.union a' b'
     where
           a' = Set.map (mergeSub' al) be
           b' = Set.filter (not.subColides be) $ Set.fromList [al]
@@ -32,14 +36,16 @@ mergeSub al be = Set.union a' b'
 mergeSub' :: Sub -> Sub -> Sub
 mergeSub' subs (s :\: Var b) = sub subs s :\: Var b
 
+nubSub :: Set.Set Sub -> Set.Set Sub
+nubSub = Set.filter (\( t :\: b) -> t /= b )
 
-subColides ::  Set.Set Sub-> Sub -> Bool
-subColides o (s :\: source) = Set.member source vars
-    where vars = getVars o
+
+
+
 
 
 getVar :: Sub -> Exp
-getVar (a :\: b) = b  
+getVar (a :\: b) = b
 
 getVars :: Set.Set Sub -> Set.Set Exp
 getVars = Set.map getVar
